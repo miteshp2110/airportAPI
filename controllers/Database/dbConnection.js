bc977d8d02f8d9a73f7a0113126fc8c3cad014f3
Mitesh Paliwal <113752545+miteshp2110@users.noreply.github.com>
2024-03-19 17:50:20 +0530

    Update dbConnection.js

--- controllers/Database/dbConnection.js
+++ controllers/Database/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 async function dbConnectivity(){
     try{
-        await mongoose.connect('mongodb+srv://MiteshPC:Mi12te34@cluster0.ojmszjm.mongodb.net/airportsdata')
+        await mongoose.connect('yourDatabaseURI')
 
         // const dbInstance=mongoose.connection
         // console.log("insttance: ",dbInstance)
